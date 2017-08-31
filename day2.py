2174987058a02085bcd90da1cb2e7363e6814258
Ashutoshh <asashuthegreat4@gmail.com>
2017-08-31 18:45:27 +0530

    Add files via upload
    30th august 2017

---
+++ day2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env  python
+
+import time
+print  "Hello Python 2"
+
+x=raw_input("type  number first :   ")
+
+y=raw_input("type  number second :   ")
+
+print   int(x)  +  int(y)
+
+
+#time.sleep(3)
+
+raw_input("press enter  to quit ")
+
+
